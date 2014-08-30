2dd073ffd8a104e8a9e6290ef1dc14a63229c040
Tom Molesworth <cpan@entitymodel.com>
2014-08-30 23:38:45 +0000

    Import of TEAM/Tickit-DSL-0.019 from CPAN.
    gitpan-cpan-distribution: Tickit-DSL
    gitpan-cpan-version:      0.019
    gitpan-cpan-path:         TEAM/Tickit-DSL-0.019.tar.gz
    gitpan-cpan-author:       TEAM
    gitpan-cpan-maturity:     released

--- lib/Tickit/DSL.pm
+++ lib/Tickit/DSL.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use parent qw(Exporter);
 
-our $VERSION = '0.018';
+our $VERSION = '0.019';
 
 =head1 NAME
 
@@ -13,7 +13,7 @@ Tickit::DSL - domain-specific language for Tickit terminal apps
 
 =head1 VERSION
 
-Version 0.018
+Version 0.019
 
 =head1 SYNOPSIS
 
